54e51ac453e4b0b2b2bf8b0b611a553a84974c93
Michiel Vos <michie1@users.noreply.github.com>
2023-03-26 09:59:26 +0200

    Do not build again after build phase

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,5 @@ ssh hetzner 'cd ~/convert && git pull' &&
 ssh hetzner 'tar -xf /home/michiel/convert/build.tar -C /home/michiel/convert' &&
 ssh hetzner 'rm ~/convert/build.tar' &&
 ssh hetzner 'cd ~/convert && docker-compose build' &&
-ssh hetzner 'cd ~/convert && docker-compose up --build -d'
+ssh hetzner 'cd ~/convert && docker-compose up -d'
 rm build.tar
